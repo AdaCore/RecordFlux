520203dde8606afa8feb2c47c47a788a1e4c0794
Tobias Reiher <treiher@users.noreply.github.com>
2018-02-23 16:01:39 +0100

    Split parser and tests into separate files

--- parser.py
+++ parser.py
@@ -2,15 +2,9 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring, line-too-long
 
-import sys
-import unittest
-
-from pyparsing import alphanums, nums, Word, Keyword, Literal, Optional, StringEnd, QuotedString, Forward, ZeroOrMore, Regex, WordStart, WordEnd
+from pyparsing import alphanums, nums, Forward, Keyword, Literal, Optional, QuotedString, Regex, StringEnd, Word, WordEnd, WordStart, ZeroOrMore
 
 
-#
-# Parser
-#
 class Parser:
     def __init__(self, basedir='.'):
         # pylint: disable=too-many-locals, too-many-statements, expression-not-assigned
@@ -165,68 +159,3 @@ class Parser:
 
     def data(self):
         return self.__data
-
-
-#
-# Parser test cases
-#
-class TestParser(unittest.TestCase):
-    # pylint: disable=too-many-public-methods
-    def setUp(self):
-        self.testdir = "tests"
-
-    def fullpath(self, testfile):
-        return self.testdir + "/" + testfile
-
-    def parse(self, filename):
-        parser = Parser()
-        parser.parse(self.fullpath(filename))
-        return parser
-
-    def assert_data(self, filename, data):
-        parser = self.parse(filename)
-        self.assertEqual(parser.data(), data, self.fullpath(filename))
-
-    def test_empty_file(self):
-        self.assert_data("empty_file.rflx", [])
-
-    def test_comment_only(self):
-        self.assert_data("comment_only.rflx", [])
-
-    def test_package(self):
-        self.assert_data("package.rflx", [])
-
-    def test_derived_type(self):
-        self.assert_data("derived_type.rflx", [])
-
-    def test_integer_type(self):
-        self.assert_data("integer_type.rflx", [])
-
-    def test_enumeration_type(self):
-        self.assert_data("enumeration_type.rflx", [])
-
-    def test_array_type(self):
-        self.assert_data("array_type.rflx", [])
-
-    def test_record_type(self):
-        self.assert_data("record_type.rflx", [])
-
-    def test_record_type_with_slice(self):
-        self.assert_data("record_type_with_slice.rflx", [])
-
-    def test_record_type_with_variant(self):
-        self.assert_data("record_type_with_variant.rflx", [])
-
-    def test_record_type_with_aspect(self):
-        self.assert_data("record_type_with_aspect.rflx", [])
-
-    def test_ethernet(self):
-        self.assert_data("ethernet.rflx", [])
-
-
-if __name__ == "__main__":
-    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestParser)
-    TESTRESULT = unittest.TextTestRunner().run(SUITE)
-
-    if TESTRESULT.failures or TESTRESULT.errors:
-        sys.exit(1)
---
+++ tests/test_parser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+
+import unittest
+
+from parser import Parser
+
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.testdir = "tests"
+
+    def fullpath(self, testfile):
+        return self.testdir + "/" + testfile
+
+    def parse(self, filename):
+        parser = Parser()
+        parser.parse(self.fullpath(filename))
+        return parser
+
+    def assert_data(self, filename, data):
+        parser = self.parse(filename)
+        self.assertEqual(parser.data(), data, self.fullpath(filename))
+
+    def test_empty_file(self):
+        self.assert_data("empty_file.rflx", [])
+
+    def test_comment_only(self):
+        self.assert_data("comment_only.rflx", [])
+
+    def test_package(self):
+        self.assert_data("package.rflx", [])
+
+    def test_derived_type(self):
+        self.assert_data("derived_type.rflx", [])
+
+    def test_integer_type(self):
+        self.assert_data("integer_type.rflx", [])
+
+    def test_enumeration_type(self):
+        self.assert_data("enumeration_type.rflx", [])
+
+    def test_array_type(self):
+        self.assert_data("array_type.rflx", [])
+
+    def test_record_type(self):
+        self.assert_data("record_type.rflx", [])
+
+    def test_record_type_with_slice(self):
+        self.assert_data("record_type_with_slice.rflx", [])
+
+    def test_record_type_with_variant(self):
+        self.assert_data("record_type_with_variant.rflx", [])
+
+    def test_record_type_with_aspect(self):
+        self.assert_data("record_type_with_aspect.rflx", [])
+
+    def test_ethernet(self):
+        self.assert_data("ethernet.rflx", [])
+
+
+if __name__ == "__main__":
+    unittest.main()
