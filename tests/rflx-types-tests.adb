51fa98049cf6f3eba3b63fd64ea86e6f2a234832
Tobias Reiher <treiher@users.noreply.github.com>
2018-12-04 15:33:19 +0100

    Extend tests for type conversion
    Related to #25

--- tests/rflx-types-tests.adb
+++ tests/rflx-types-tests.adb
@@ -33,7 +33,7 @@ package body RFLX.Types.Tests is
      SPARK_Mode, Pre => True
    is
       pragma Unreferenced (T);
-      Buffer : Bytes := (255, 0);
+      Buffer : Bytes := (255, 255, 0);
 
       type UINT13 is mod 2**13;
       function Convert_To_UINT13 is new Convert_To_Mod (UINT13);
@@ -43,26 +43,36 @@ package body RFLX.Types.Tests is
       function Convert_To_UINT8 is new Convert_To_Mod (UINT8);
       R8 : UINT8;
    begin
-      R13 := Convert_To_UINT13 (Buffer, 0);
+      R13 := Convert_To_UINT13 (Buffer (Buffer'First + 1 .. Buffer'Last), 0);
       Assert (R13'Image, UINT13'Image (7936), "Invalid conversion with offset 0");
-      R13 := Convert_To_UINT13 (Buffer, 1);
+      R13 := Convert_To_UINT13 (Buffer (Buffer'First + 1 .. Buffer'Last), 1);
       Assert (R13'Image, UINT13'Image (8064), "Invalid conversion with offset 1");
-      R13 := Convert_To_UINT13 (Buffer, 2);
+      R13 := Convert_To_UINT13 (Buffer (Buffer'First + 1 .. Buffer'Last), 2);
       Assert (R13'Image, UINT13'Image (8128), "Invalid conversion with offset 2");
+      R13 := Convert_To_UINT13 (Buffer (Buffer'First + 1 .. Buffer'Last), 3);
+      Assert (R13'Image, UINT13'Image (8160), "Invalid conversion with offset 3");
+      R13 := Convert_To_UINT13 (Buffer, 4);
+      Assert (R13'Image, UINT13'Image (8176), "Invalid conversion with offset 4");
+      R13 := Convert_To_UINT13 (Buffer, 5);
+      Assert (R13'Image, UINT13'Image (8184), "Invalid conversion with offset 5");
+      R13 := Convert_To_UINT13 (Buffer, 6);
+      Assert (R13'Image, UINT13'Image (8188), "Invalid conversion with offset 6");
+      R13 := Convert_To_UINT13 (Buffer, 7);
+      Assert (R13'Image, UINT13'Image (8190), "Invalid conversion with offset 7");
 
       R8 := Convert_To_UINT8 (Buffer (Buffer'Last .. Buffer'Last), 0);
       Assert (R8'Image, UINT8'Image (0), "Invalid conversion with offset 0");
-      R8 := Convert_To_UINT8 (Buffer, 1);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 1);
       Assert (R8'Image, UINT8'Image (128), "Invalid conversion with offset 1");
-      R8 := Convert_To_UINT8 (Buffer, 2);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 2);
       Assert (R8'Image, UINT8'Image (192), "Invalid conversion with offset 2");
-      R8 := Convert_To_UINT8 (Buffer, 3);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 3);
       Assert (R8'Image, UINT8'Image (224), "Invalid conversion with offset 3");
-      R8 := Convert_To_UINT8 (Buffer, 4);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 4);
       Assert (R8'Image, UINT8'Image (240), "Invalid conversion with offset 4");
-      R8 := Convert_To_UINT8 (Buffer, 6);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 6);
       Assert (R8'Image, UINT8'Image (252), "Invalid conversion with offset 6");
-      R8 := Convert_To_UINT8 (Buffer, 7);
+      R8 := Convert_To_UINT8 (Buffer (Buffer'First + 1 .. Buffer'Last), 7);
       Assert (R8'Image, UINT8'Image (254), "Invalid conversion with offset 7");
       R8 := Convert_To_UINT8 (Buffer (Buffer'First .. Buffer'First), 0);
       Assert (R8'Image, UINT8'Image (255), "Invalid conversion with offset 0");
