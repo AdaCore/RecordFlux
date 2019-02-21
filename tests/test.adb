33c479ff5ab25e96f57c87e6b552bce58de8f261
Tobias Reiher <treiher@users.noreply.github.com>
2019-02-21 12:47:54 +0100

    Make AUnit fail on unsuccessful tests

--- tests/test.adb
+++ tests/test.adb
@@ -1,10 +1,18 @@
+with Ada.Command_Line;
 with AUnit.Reporter.Text;
 with AUnit.Run;
+use type Aunit.Status;
+
 with Test_Suite;
 
 procedure Test is
-   procedure Runner is new AUnit.Run.Test_Runner (Test_Suite.Suite);
+   function Run is new Aunit.Run.Test_Runner_With_Status (Test_Suite.Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
+   Status : Aunit.Status := Run (Reporter);
 begin
-   Runner (Reporter);
+   if Status = Aunit.Success then
+      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
+   else
+      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
+   end if;
 end Test;
