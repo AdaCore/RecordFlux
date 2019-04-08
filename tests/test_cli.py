19502c325f776205362ace5ba2b872961f523949
Tobias Reiher <reiher@componolit.com>
2019-04-08 14:14:57 +0200

    Fix warnings in CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,7 @@ from rflx import cli
 from rflx.model import ModelError
 
 
-class TestRflx(TestCase):
+class TestCLI(TestCase):
     def setUp(self) -> None:
         self.maxDiff = None  # pylint: disable=invalid-name
 
@@ -35,19 +35,19 @@ class TestRflx(TestCase):
                          r'error: file not found: "non-existent file"$')
 
     def test_main_generate(self) -> None:
-        tmpdir = TemporaryDirectory()
-        self.assertEqual(cli.main(['rflx', 'generate', 'specs/tlv.rflx', tmpdir.name]),
-                         0)
+        with TemporaryDirectory() as tmpdir:
+            self.assertEqual(cli.main(['rflx', 'generate', 'specs/tlv.rflx', tmpdir]),
+                             0)
 
     def test_main_generate_prefix(self) -> None:
-        tmpdir = TemporaryDirectory()
-        self.assertEqual(cli.main(['rflx', 'generate', '-p', '', 'specs/tlv.rflx', tmpdir.name]),
-                         0)
+        with TemporaryDirectory() as tmpdir:
+            self.assertEqual(cli.main(['rflx', 'generate', '-p', '', 'specs/tlv.rflx', tmpdir]),
+                             0)
 
     def test_main_generate_no_output_files(self) -> None:
-        tmpdir = TemporaryDirectory()
-        self.assertEqual(cli.main(['rflx', 'generate', '-d', 'tests/package.rflx', tmpdir.name]),
-                         0)
+        with TemporaryDirectory() as tmpdir:
+            self.assertEqual(cli.main(['rflx', 'generate', '-d', 'tests/package.rflx', tmpdir]),
+                             0)
 
     def test_main_generate_non_existent_directory(self) -> None:
         self.assertRegex(str(cli.main(['rflx', 'generate', 'specs/tlv.rflx',
@@ -55,16 +55,16 @@ class TestRflx(TestCase):
                          r'error: directory not found: "non-existent directory"$')
 
     def test_main_generate_missing_template_direcotry(self) -> None:
-        tmpdir = TemporaryDirectory()
-        with mock.patch('pkg_resources.resource_filename', lambda *x: 'non-existent directory'):
-            self.assertRegex(str(cli.main(['rflx', 'generate', '-l', tmpdir.name])),
-                             r'internal error: library directory not found$')
+        with TemporaryDirectory() as tmpdir:
+            with mock.patch('pkg_resources.resource_filename', lambda *x: 'non-existent directory'):
+                self.assertRegex(str(cli.main(['rflx', 'generate', '-l', tmpdir])),
+                                 r'internal error: library directory not found$')
 
     def test_main_generate_missing_template_files(self) -> None:
-        tmpdir = TemporaryDirectory()
-        with mock.patch('pkg_resources.resource_filename', lambda *x: tmpdir.name):
-            self.assertRegex(str(cli.main(['rflx', 'generate', '-l', tmpdir.name])),
-                             r'internal error: library file not found')
+        with TemporaryDirectory() as tmpdir:
+            with mock.patch('pkg_resources.resource_filename', lambda *x: tmpdir):
+                self.assertRegex(str(cli.main(['rflx', 'generate', '-l', tmpdir])),
+                                 r'internal error: library file not found')
 
 
 def raise_model_error() -> None:
