c8e3376aa2c3cd22237f78fbb1e051ebf9f45de9
Tobias Reiher <reiher@componolit.com>
2019-10-08 12:18:47 +0200

    Fix proof of Extract function for dynamically determined offsets
    Related to #25

--- generated/rflx-types.ads
+++ generated/rflx-types.ads
@@ -63,7 +63,7 @@ is
      Pre => (Offset_Type'Pos (Offset) + Value_Type'Size - 1) / Element_Type'Size < Data'Length
              and then (Offset_Type'Pos (Offset) + Value_Type'Size - 1) / Element_Type'Size <= Natural'Size
              and then Natural (((Offset_Type'Pos (Offset) + Value_Type'Size - 1) / Element_Type'Size) * Element_Type'Size) < Long_Integer'Size - 1
-             and then 2**(Element_Type'Size - Natural (Offset_Type'Pos (Offset) mod Element_Type'Size)) <= Long_Integer'Last;
+             and then (Element_Type'Size - Natural (Offset_Type'Pos (Offset) mod Element_Type'Size)) < Long_Integer'Size - 1;
 
    pragma Warnings (Off, "precondition is statically false");
 
