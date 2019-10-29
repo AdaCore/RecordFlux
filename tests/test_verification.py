48b77beff7787084a8fa991184d003fdd1f1f527
Alexander Senier <senier@componolit.com>
2019-10-29 18:45:36 +0100

    Add type condition test cases
    Ref. #83

--- tests/test_verification.py
+++ tests/test_verification.py
@@ -172,3 +172,60 @@ class TestVerification(unittest.TestCase):
             end Foo;
             """,
             r'^contradicting condition 0 from field "F1" to "F2"')
+
+    def test_invalid_type_condition_range_low(self) -> None:
+        self.assert_parse_exception_string(
+            """
+            package Foo is
+                type Element is range 100..1000 with Size => 32;
+                type Bar is
+                    message
+                        F1 : Element then F2 if F1 < 100;
+                        F2 : Element;
+                    end message;
+            end Foo;
+            """,
+            r'^unreachable field "F2"')
+
+    def test_invalid_type_condition_range_high(self) -> None:
+        self.assert_parse_exception_string(
+            """
+            package Foo is
+                type Element is range 5..50000 with Size => 32;
+                type Bar is
+                    message
+                        F1 : Element then F2 if F1 > 60000;
+                        F2 : Element;
+                    end message;
+            end Foo;
+            """,
+            r'^unreachable field "F2"')
+
+    def test_invalid_type_condition_modular(self) -> None:
+        self.assert_parse_exception_string(
+            """
+            package Foo is
+                type Element is mod 2**8;
+                type Bar is
+                    message
+                        F1 : Element then F2 if F1 > 1000;
+                        F2 : Element;
+                    end message;
+            end Foo;
+            """,
+            r'^unreachable field "F2"')
+
+    def test_invalid_type_condition_enum(self) -> None:
+        self.assert_parse_exception_string(
+            """
+            package Foo is
+                type Element1 is (E1, E2, E3) with Size => 8;
+                type Element2 is (E4, E5, E6) with Size => 8;
+                type Bar is
+                    message
+                        F1 : Element1 then F2 if F1 = E4;
+                        F2 : Element2;
+                    end message;
+            end Foo;
+            """,
+            r'^contradicting condition 0 from field "F1" to "F2"')
