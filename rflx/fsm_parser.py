ecef22a740e0ac5923afccd68b503e8abab53b4f
Alexander Senier <senier@componolit.com>
2019-12-16 08:07:08 +0100

    Implement conjunction and disjunction

--- rflx/fsm_parser.py
+++ rflx/fsm_parser.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from pyparsing import Keyword, Literal, StringEnd, Token, infixNotation, opAssoc
+from pyparsing import Forward, Keyword, Literal, StringEnd, Token, infixNotation, opAssoc
 
-from rflx.expression import FALSE, TRUE, Equal, Expr, NotEqual, Variable
+from rflx.expression import FALSE, TRUE, And, Equal, Expr, NotEqual, Or, Variable
 from rflx.fsm_expression import Valid
 from rflx.identifier import ID
 from rflx.parser.grammar import boolean_literal, qualified_identifier
@@ -20,6 +20,16 @@ class FSMParser:
         return NotEqual(t[0], t[2])
 
     @classmethod
+    def __parse_conjunction(cls, tokens: List[List[Expr]]) -> Expr:
+        t = tokens[0]
+        return And(t[0], t[2])
+
+    @classmethod
+    def __parse_disjunction(cls, tokens: List[List[Expr]]) -> Expr:
+        t = tokens[0]
+        return Or(t[0], t[2])
+
+    @classmethod
     def expression(cls) -> Token:
         literal = boolean_literal()
         literal.setParseAction(lambda t: TRUE if t[0] == "True" else FALSE)
@@ -30,17 +40,20 @@ class FSMParser:
         valid = identifier() + Literal("'") - Keyword("Valid")
         valid.setParseAction(lambda t: Valid(t[0]))
 
-        simple_expression = literal | valid | identifier
+        expr = Forward()
+        simple_expr = literal | valid | identifier | expr
 
-        equation = infixNotation(
-            simple_expression,
+        expr <<= infixNotation(
+            simple_expr,
             [
                 (Keyword("="), 2, opAssoc.LEFT, cls.__parse_equation),
                 (Keyword("/="), 2, opAssoc.LEFT, cls.__parse_inequation),
+                (Keyword("and"), 2, opAssoc.LEFT, cls.__parse_conjunction),
+                (Keyword("or"), 2, opAssoc.LEFT, cls.__parse_disjunction),
             ],
         )
 
-        return equation | simple_expression
+        return expr
 
     @classmethod
     def condition(cls) -> Token:
--- tests/test_fsm_parse.py
+++ tests/test_fsm_parse.py
@@ -1,4 +1,4 @@
-from rflx.expression import Equal, NotEqual, Variable
+from rflx.expression import And, Equal, NotEqual, Or, Variable
 from rflx.fsm_expression import Valid
 from rflx.fsm_parser import FSMParser
 
@@ -16,3 +16,22 @@ def test_simple_inequation() -> None:
 def test_valid() -> None:
     result = FSMParser.condition().parseString("Something'Valid")[0]
     assert result == Valid(Variable("Something"))
+
+
+def test_conjunction_valid() -> None:
+    result = FSMParser.condition().parseString("Foo'Valid and Bar'Valid")[0]
+    assert result == And(Valid(Variable("Foo")), Valid(Variable("Bar")))
+
+
+def test_conjunction() -> None:
+    result = FSMParser.condition().parseString("Foo = Bar and Bar /= Baz")[0]
+    assert result == And(
+        Equal(Variable("Foo"), Variable("Bar")), NotEqual(Variable("Bar"), Variable("Baz"))
+    )
+
+
+def test_disjunction() -> None:
+    result = FSMParser.condition().parseString("Foo = Bar or Bar /= Baz")[0]
+    assert result == Or(
+        Equal(Variable("Foo"), Variable("Bar")), NotEqual(Variable("Bar"), Variable("Baz"))
+    )
