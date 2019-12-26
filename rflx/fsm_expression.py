e1d9bc6d0fab5613fc71ac4341d04d7b052160ac
Alexander Senier <senier@componolit.com>
2019-12-26 20:07:23 +0100

    Support quantifiers

--- rflx/fsm_expression.py
+++ rflx/fsm_expression.py
@@ -1,10 +1,72 @@
 import z3
 
-from rflx.expression import Attribute, Expr, Not, Precedence, Relation
+from rflx.error import Location
+from rflx.expression import Attribute, Expr, Not, Precedence, Relation, Variable
 
 
 class Valid(Attribute):
     pass
+
+
+class Quantifier(Expr):
+    def __init__(
+        self, quantifier: Variable, iteratable: Expr, predicate: Expr, location: Location = None
+    ) -> None:
+        super().__init__(location)
+        self.__quantifier = quantifier
+        self.__iterable = iteratable
+        self.__predicate = predicate
+        self.symbol: str = ""
+
+    def __repr__(self) -> str:
+        return f"for {self.symbol} {self.__quantifier} in {self.__iterable} => {self.__predicate}"
+
+    def __neg__(self) -> "Expr":
+        raise NotImplementedError
+
+    @property
+    def precedence(self) -> Precedence:
+        raise NotImplementedError
+
+    def simplified(self) -> "Expr":
+        raise NotImplementedError
+
+    def z3expr(self) -> z3.ExprRef:
+        raise NotImplementedError
+
+
+class ForSome(Quantifier):
+    symbol: str = "some"
+
+    def __neg__(self) -> "Expr":
+        raise NotImplementedError
+
+    @property
+    def precedence(self) -> Precedence:
+        raise NotImplementedError
+
+    def simplified(self) -> "Expr":
+        raise NotImplementedError
+
+    def z3expr(self) -> z3.ExprRef:
+        raise NotImplementedError
+
+
+class ForAll(Quantifier):
+    symbol: str = "all"
+
+    def __neg__(self) -> "Expr":
+        raise NotImplementedError
+
+    @property
+    def precedence(self) -> Precedence:
+        raise NotImplementedError
+
+    def simplified(self) -> "Expr":
+        raise NotImplementedError
+
+    def z3expr(self) -> z3.ExprRef:
+        raise NotImplementedError
 
 
 class Contains(Relation):
