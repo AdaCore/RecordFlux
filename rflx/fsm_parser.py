530f774156a68284df970487ba7c6043cb80c500
Alexander Senier <senier@componolit.com>
2019-12-26 17:09:53 +0100

    Fix conjunction/disjunction handling

--- rflx/fsm_parser.py
+++ rflx/fsm_parser.py
@@ -5,7 +5,7 @@ from pyparsing import Keyword, Literal, StringEnd, Token, infixNotation, opAssoc
 from rflx.expression import FALSE, TRUE, And, Equal, Expr, NotEqual, Or, Variable
 from rflx.fsm_expression import Contains, NotContains, Valid
 from rflx.identifier import ID
-from rflx.parser.grammar import boolean_literal, qualified_identifier
+from rflx.parser.grammar import boolean_literal, numeric_literal, qualified_identifier
 
 
 class FSMParser:
@@ -22,12 +22,12 @@ class FSMParser:
     @classmethod
     def __parse_conjunction(cls, tokens: List[List[Expr]]) -> Expr:
         t = tokens[0]
-        return And(t[0], t[2])
+        return And(*t)
 
     @classmethod
     def __parse_disjunction(cls, tokens: List[List[Expr]]) -> Expr:
         t = tokens[0]
-        return Or(t[0], t[2])
+        return Or(*t)
 
     @classmethod
     def __parse_in(cls, tokens: List[List[Expr]]) -> Expr:
@@ -51,7 +51,7 @@ class FSMParser:
         valid.setParseAction(lambda t: Valid(t[0]))
 
         equation = infixNotation(
-            literal | valid | identifier,
+            numeric_literal() | literal | valid | identifier,
             [
                 (Keyword("="), 2, opAssoc.LEFT, cls.__parse_equation),
                 (Keyword("/="), 2, opAssoc.LEFT, cls.__parse_inequation),
@@ -63,8 +63,8 @@ class FSMParser:
         result = infixNotation(
             equation,
             [
-                (Keyword("and"), 2, opAssoc.LEFT, cls.__parse_conjunction),
-                (Keyword("or"), 2, opAssoc.LEFT, cls.__parse_disjunction),
+                (Keyword("and").suppress(), 2, opAssoc.LEFT, cls.__parse_conjunction),
+                (Keyword("or").suppress(), 2, opAssoc.LEFT, cls.__parse_disjunction),
             ],
         )
         return result
