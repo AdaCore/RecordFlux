3065878b7541db30c27836e38c087a491c90c211
Alexander Senier <senier@componolit.com>
2020-01-27 19:01:20 +0100

    Implement type conversion

--- rflx/fsm_expression.py
+++ rflx/fsm_expression.py
@@ -21,14 +21,14 @@ class Quantifier(Expr):
     def __repr__(self) -> str:
         return f"for {self.symbol} {self.__quantifier} in {self.__iterable} => {self.__predicate}"
 
-    def __neg__(self) -> "Expr":
+    def __neg__(self) -> Expr:
         raise NotImplementedError
 
     @property
     def precedence(self) -> Precedence:
-        raise NotImplementedError
+        return Precedence.undefined
 
-    def simplified(self) -> "Expr":
+    def simplified(self) -> Expr:
         raise NotImplementedError
 
     def z3expr(self) -> z3.ExprRef:
@@ -38,14 +38,14 @@ class Quantifier(Expr):
 class ForSome(Quantifier):
     symbol: str = "some"
 
-    def __neg__(self) -> "Expr":
+    def __neg__(self) -> Expr:
         raise NotImplementedError
 
     @property
     def precedence(self) -> Precedence:
-        raise NotImplementedError
+        return Precedence.undefined
 
-    def simplified(self) -> "Expr":
+    def simplified(self) -> Expr:
         raise NotImplementedError
 
     def z3expr(self) -> z3.ExprRef:
@@ -55,14 +55,14 @@ class ForSome(Quantifier):
 class ForAll(Quantifier):
     symbol: str = "all"
 
-    def __neg__(self) -> "Expr":
+    def __neg__(self) -> Expr:
         raise NotImplementedError
 
     @property
     def precedence(self) -> Precedence:
-        raise NotImplementedError
+        return Precedence.undefined
 
-    def simplified(self) -> "Expr":
+    def simplified(self) -> Expr:
         raise NotImplementedError
 
     def z3expr(self) -> z3.ExprRef:
@@ -79,7 +79,7 @@ class Contains(Relation):
 
     @property
     def precedence(self) -> Precedence:
-        return Precedence.set_operator
+        return Precedence.undefined
 
     def z3expr(self) -> z3.ExprRef:
         raise NotImplementedError
@@ -95,7 +95,53 @@ class NotContains(Relation):
 
     @property
     def precedence(self) -> Precedence:
-        return Precedence.set_operator
+        return Precedence.undefined
+
+    def z3expr(self) -> z3.ExprRef:
+        raise NotImplementedError
+
+
+class Convert(Expr):
+    def __init__(self, expression: Expr, target: Variable, location: Location = None) -> None:
+        super().__init__(location)
+        self.__expression = expression
+        self.__type = target
+
+    def __repr__(self) -> str:
+        return f"{self.__type} ({self.__expression})"
+
+    def __neg__(self) -> Expr:
+        raise NotImplementedError
+
+    def simplified(self) -> Expr:
+        raise NotImplementedError
+
+    @property
+    def precedence(self) -> Precedence:
+        return Precedence.undefined
+
+    def z3expr(self) -> z3.ExprRef:
+        raise NotImplementedError
+
+
+class Field(Expr):
+    def __init__(self, expression: Expr, field: str, location: Location = None) -> None:
+        super().__init__(location)
+        self.__expression = expression
+        self.__field = field
+
+    def __repr__(self) -> str:
+        return f"{self.__expression}.{self.__field}"
+
+    def __neg__(self) -> Expr:
+        raise NotImplementedError
+
+    def simplified(self) -> Expr:
+        raise NotImplementedError
+
+    @property
+    def precedence(self) -> Precedence:
+        return Precedence.undefined
 
     def z3expr(self) -> z3.ExprRef:
         raise NotImplementedError
