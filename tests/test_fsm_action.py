4987d5b01be48e81ef367b022909d53979b07c06
Alexander Senier <senier@componolit.com>
2020-02-18 21:35:06 +0100

    Support subprogram calls in actions
    Closes #135

--- tests/test_fsm_action.py
+++ tests/test_fsm_action.py
@@ -1,4 +1,5 @@
 from rflx.expression import Variable
+from rflx.fsm_expression import SubprogramCall
 from rflx.fsm_parser import FSMParser
 from rflx.statement import Assignment
 
@@ -6,3 +7,9 @@ from rflx.statement import Assignment
 def test_simple_assignment() -> None:
     result = FSMParser.action().parseString("Foo := Bar")[0]
     assert result == Assignment("Foo", Variable("Bar"))
+
+
+def test_simple_subprogram_call() -> None:
+    result = FSMParser.action().parseString("Sub (Arg)")[0]
+    expected = SubprogramCall("Sub", [Variable("Arg")])
+    assert result == expected
