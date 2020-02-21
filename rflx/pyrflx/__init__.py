fe187d80a87875e32b18f2300a1766715dc1151f
Johannes Kliemann <kliemann@componolit.com>
2020-02-21 17:56:39 +0100

    build field path through graph
    ref #128

--- rflx/pyrflx/__init__.py
+++ rflx/pyrflx/__init__.py
@@ -1,3 +1,10 @@
 from .pyrflx import PyRFLX
-from .typevalue import NotInitializedError, TypeValue, ModularValue, RangeValue, EnumValue, OpaqueValue
-from .message import Message
+from .typevalue import (
+    NotInitializedError,
+    TypeValue,
+    ModularValue,
+    RangeValue,
+    EnumValue,
+    OpaqueValue,
+)
+from .message import Message, Field
