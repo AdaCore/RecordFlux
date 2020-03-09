86325b56cce0eee420ece5af4488644bc8753454
Robin Senn <senn@componolit.com>
2020-03-09 14:43:17 +0100

    WIP issue #163 changes according to review

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -228,27 +228,6 @@ class TestExpression(unittest.TestCase):  # pylint: disable=too-many-public-meth
             Add(Variable("X"), Variable("Y")),
         )
 
-        # 3.simplified(1)
-        self.assertEqual(
-            Sub(Variable("X"), Variable("Y")).simplified({Variable("X"): Variable("Z")}),
-            Add(Variable("Z"), -Variable("Y")),
-        )
-
-        # 3.simplified(1).simplified(2) = 4
-        self.assertEqual(
-            Sub(Variable("Z"), Variable("Y")).simplified(
-                {Variable("Z"): Add(Variable("Y"), Variable("Q"))}
-            ),
-            Add(Add(Variable("Y"), Variable("Q")), -Variable("Y")),
-        )
-
-        # 4.simplified() = TRUE
-        self.assertTrue(
-            Equal(Variable("Q"), Sub(Add(Variable("Y"), Variable("Q")), Variable("Y")))
-            .simplified()
-            .simplified()
-        )
-
     def test_add_lt(self) -> None:
         self.assertEqual(Add(Variable("X"), Number(1)) < Add(Variable("X"), Number(2)), True)
         self.assertEqual(Add(Variable("X"), Number(2)) < Add(Variable("X"), Number(2)), False)
@@ -314,6 +293,26 @@ class TestExpression(unittest.TestCase):  # pylint: disable=too-many-public-meth
         self.assertEqual(Sub(Number(6), Number(2)).simplified(), Number(4))
         self.assertEqual(
             Sub(Variable("X"), Variable("Y")).simplified(), Add(Variable("X"), Variable("Y", True))
+        )
+        # 3.simplified(1)
+        self.assertEqual(
+            Sub(Variable("X"), Variable("Y")).simplified({Variable("X"): Variable("Z")}),
+            Add(Variable("Z"), -Variable("Y")),
+        )
+
+        # 3.simplified(1).simplified(2) = 4
+        self.assertEqual(
+            Sub(Variable("Z"), Variable("Y")).simplified(
+                {Variable("Z"): Add(Variable("Y"), Variable("Q"))}
+            ),
+            Add(Add(Variable("Y"), Variable("Q")), -Variable("Y")),
+        )
+
+        # 4.simplified() = TRUE
+        self.assertTrue(
+            Equal(Variable("Q"), Sub(Add(Variable("Y"), Variable("Q")), Variable("Y")))
+            .simplified()
+            .simplified()
         )
 
     def test_div_neg(self) -> None:
