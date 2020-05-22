6b6698bf0913b3465eccf2bf3f231ef1798c6e51
Alexander Senier <senier@componolit.com>
2020-05-22 19:54:29 +0200

    Extend error handling
    Ref. #248

---
+++ rflx/error.py
@@ -0,0 +1,140 @@
+from enum import Enum, auto
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+
+class Location:
+    def __init__(
+        self,
+        start: Tuple[int, int],
+        filename: Path = None,
+        end: Tuple[int, int] = None,
+        verbose: bool = False,
+    ):
+        self.__filename = filename
+        self.__start = start
+        self.__end = end
+        self.__verbose = verbose
+
+    def __str__(self) -> str:
+        def linecol_str(linecol: Tuple[int, int]) -> str:
+            return f"{linecol[0]}:{linecol[1]}"
+
+        start = f":{linecol_str(self.__start)}" if self.__start else ""
+        end = f"-{linecol_str(self.__end)}" if self.__end and self.__verbose else ""
+        return f"{self.__filename}{start}{end}"
+
+    def set_filename(self, filename: Path) -> None:
+        self.__filename = filename
+
+    @property
+    def start(self) -> Tuple[int, int]:
+        return self.__start
+
+    @property
+    def end(self) -> Optional[Tuple[int, int]]:
+        return self.__end
+
+
+class Subsystem(Enum):
+    CORE = auto()
+    PARSER = auto()
+    MODEL = auto()
+    CLI = auto()
+    INTERNAL = auto()
+
+    def __str__(self) -> str:
+        return str.lower(self.name)
+
+
+class Severity(Enum):
+    NONE = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+
+    def __gt__(self, other: "Severity") -> bool:
+        # ISSUE: PyCQA/pylint#2306
+        value = int(self.value)
+        return value > other.value
+
+    def __str__(self) -> str:
+        return str.lower(self.name)
+
+
+class RecordFluxError(Exception):
+    class Entry:
+        def __init__(
+            self, message: str, subsystem: Subsystem, severity: Severity, location: Location = None
+        ):
+            self.__message = message
+            self.__subsystem = subsystem
+            self.__severity = severity
+            self.__location = location
+
+        @property
+        def message(self) -> str:
+            return self.__message
+
+        @property
+        def subsystem(self) -> Subsystem:
+            return self.__subsystem
+
+        @property
+        def severity(self) -> Severity:
+            return self.__severity
+
+        @property
+        def location(self) -> Optional[Location]:
+            return self.__location
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.__errors: List[RecordFluxError.Entry] = []
+
+    def add(
+        self, message: str, subsystem: Subsystem, severity: Severity, location: Location = None
+    ) -> None:
+        self.__errors.append(RecordFluxError.Entry(message, subsystem, severity, location))
+
+    def raise_if_above(self, severity: Severity) -> None:
+        if any([e.severity > severity for e in self.__errors]):
+            raise self
+
+    @property
+    def message(self) -> str:
+        return "\n".join(
+            f"{e.location or e.subsystem}: {e.severity}: {e.message}" for e in self.__errors
+        )
+
+
+class ParserError(RecordFluxError):
+    def __init__(self, message: str = "parser error") -> None:
+        super().__init__()
+        self.add(message, Subsystem.PARSER, Severity.ERROR)
+        self.raise_if_above(Severity.NONE)
+
+
+class InternalError(RecordFluxError):
+    def __init__(self, message: str = "internal error") -> None:
+        super().__init__()
+        self.add(message, Subsystem.INTERNAL, Severity.ERROR)
+        self.raise_if_above(Severity.NONE)
+
+
+class ModelError(RecordFluxError):
+    def __init__(self, message: str = "model error") -> None:
+        super().__init__()
+        self.add(message, Subsystem.MODEL, Severity.ERROR)
+        self.raise_if_above(Severity.NONE)
+
+
+def fail(
+    message: str,
+    subsystem: Subsystem,
+    severity: Severity = Severity.ERROR,
+    location: Location = None,
+) -> None:
+    e = RecordFluxError()
+    e.add(message, subsystem, severity, location)
+    e.raise_if_above(Severity.NONE)
