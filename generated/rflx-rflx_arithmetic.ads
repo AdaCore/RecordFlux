941b6926171b825d3fd0343ab75b7c96146df23d
Tobias Reiher <reiher@componolit.com>
2020-06-19 17:14:56 +0200

    Prevent potential overflows for non-wrapping modular type
    Ref. Componolit/Workarounds#29

--- generated/rflx-rflx_arithmetic.ads
+++ generated/rflx-rflx_arithmetic.ads
@@ -7,6 +7,18 @@ is
    type U64 is mod 2**64 with
      Annotate => (GNATprove, No_Wrap_Around);
 
+   function Pow2 (Exp : Natural) return U64 with
+     Pre =>
+       Exp < U64'Size,
+     Post =>
+       Pow2'Result = 2**Exp;
+
+   function Mod_Pow2 (Value : U64; Exp : Natural) return U64 with
+     Pre =>
+       Exp < U64'Size,
+     Post =>
+       Mod_Pow2'Result < 2**Exp;
+
    function Right_Shift (Value : U64; Value_Size : Positive; Length : Natural) return U64 with
      Pre =>
        Value_Size <= U64'Size
@@ -22,20 +34,14 @@ is
        Value_Size <= U64'Size
        and then Length < U64'Size
        and then Value_Size + Length in 1 .. U64'Size
-       and then (if Value_Size < U64'Size then Value < U64 (2)**Value_Size),
+       and then (if Value_Size < U64'Size then Value < Pow2 (Value_Size)),
      Post =>
        (if
           Value_Size + Length < U64'Size
         then
-          Left_Shift'Result <= U64 (2)**(Value_Size + Length) - U64 (2)**Length
-          and U64 (2)**(Value_Size + Length) >= U64 (2)**Length
+          Left_Shift'Result <= Pow2 (Value_Size + Length) - Pow2 (Length)
+          and Pow2 (Value_Size + Length) >= Pow2 (Length)
         else
-          Left_Shift'Result <= U64'Last - U64 (2)**Length + 1);
-
-   function Mod_Pow2 (Value : U64; Exp : Natural) return U64 with
-     Pre =>
-       Exp < U64'Size,
-     Post =>
-       Mod_Pow2'Result < 2**Exp;
+          Left_Shift'Result <= U64'Last - Pow2 (Length) + 1);
 
 end RFLX.RFLX_Arithmetic;
