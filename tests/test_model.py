f479fe7b308ede516823298232d9b01d0fe9debe
Alexander Senier <senier@componolit.com>
2020-06-28 19:28:22 +0200

    Move enumeration literal checks to model
    Ref. #298

--- tests/test_model.py
+++ tests/test_model.py
@@ -26,6 +26,7 @@ from rflx.expression import (
 from rflx.identifier import ID
 from rflx.model import (
     BOOLEAN,
+    BUILTIN_TYPES,
     FINAL,
     INITIAL,
     Array,
@@ -34,6 +35,7 @@ from rflx.model import (
     Field,
     Link,
     Message,
+    Model,
     ModularInteger,
     Opaque,
     RangeInteger,
@@ -136,6 +138,11 @@ M_SMPL_REF_DERI = UnprovenDerivedMessage(
 def assert_type_error(instance: Type, regex: str) -> None:
     with pytest.raises(RecordFluxError, match=regex):
         instance.error.propagate()
+
+
+def assert_model_error(types: Sequence[Type], regex: str) -> None:
+    with pytest.raises(RecordFluxError, match=regex):
+        Model([*BUILTIN_TYPES.values(), *types])
 
 
 def assert_message_model_error(
@@ -1206,4 +1213,83 @@ def test_invalid_enumeration_type_multiple_duplicate_elements() -> None:
         r"<stdin>:3:27: model: info: previous occurrence\n"
         r'<stdin>:3:42: model: error: duplicate element "Bar"\n'
         r"<stdin>:3:32: model: info: previous occurrence",
+    )
+
+
+def test_conflicting_literal_builtin_type() -> None:
+    assert_model_error(
+        [
+            Enumeration(
+                "P.T",
+                [
+                    (ID("E1", Location((3, 27))), Number(1)),
+                    (ID("Boolean", Location((3, 31))), Number(2)),
+                ],
+                Number(8),
+                False,
+            ),
+        ],
+        r'<stdin>:3:31: model: error: literal conflicts with type "Boolean"\n'
+        r"__BUILTINS__:0:0: model: info: conflicting type declaration",
+    )
+
+
+def test_name_conflict_between_literal_and_type() -> None:
+    assert_model_error(
+        [
+            Enumeration(
+                "P.T",
+                [
+                    (ID("Foo", Location((3, 27))), Number(1)),
+                    (ID("Bar", Location((3, 32))), Number(2)),
+                ],
+                Number(1),
+                False,
+            ),
+            ModularInteger("T.Foo", Number(256), Location((4, 16))),
+            ModularInteger("T.Bar", Number(256), Location((5, 16))),
+        ],
+        r'<stdin>:3:32: model: error: literal conflicts with type "Bar"\n'
+        r"<stdin>:5:16: model: info: conflicting type declaration\n"
+        r'<stdin>:3:27: model: error: literal conflicts with type "Foo"\n'
+        r"<stdin>:4:16: model: info: conflicting type declaration",
+    )
+
+
+def test_invalid_enumeration_type_builtin_literals() -> None:
+    assert_model_error(
+        [
+            Enumeration(
+                "P.T",
+                [("True", Number(1)), ("False", Number(2))],
+                Number(1),
+                False,
+                Location((3, 16)),
+            ),
+        ],
+        r"<stdin>:3:16: model: error: conflicting literals: False, True\n"
+        r'__BUILTINS__:0:0: model: info: previous occurrence of "False"\n'
+        r'__BUILTINS__:0:0: model: info: previous occurrence of "True"',
+    )
+
+
+def test_invalid_enumeration_type_identical_literals() -> None:
+    assert_model_error(
+        [
+            Enumeration(
+                "P.T1",
+                [("Foo", Number(1)), (ID("Bar", Location((3, 33))), Number(2))],
+                Number(1),
+                False,
+            ),
+            Enumeration(
+                "P.T2",
+                [("Bar", Number(1)), ("Baz", Number(2))],
+                Number(1),
+                False,
+                Location((4, 16)),
+            ),
+        ],
+        r"<stdin>:4:16: model: error: conflicting literals: Bar\n"
+        r'<stdin>:3:33: model: info: previous occurrence of "Bar"',
     )
