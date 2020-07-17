e1aaed477f851fe517386e0eef4098aa6518ab4d
Tobias Reiher <reiher@componolit.com>
2020-07-17 16:00:05 +0200

    Fix code generation for array with imported element type
    Ref. #349

--- generated/rflx-arrays-enumeration_vector.ads
+++ generated/rflx-arrays-enumeration_vector.ads
@@ -4,4 +4,4 @@ with RFLX.RFLX_Scalar_Sequence;
 with RFLX.Arrays;
 with RFLX.RFLX_Types;
 
-package RFLX.Arrays.Enumeration_Vector is new RFLX.RFLX_Scalar_Sequence (RFLX.RFLX_Types, Enumeration, Enumeration_Base, Valid, To_Actual, To_Base);
+package RFLX.Arrays.Enumeration_Vector is new RFLX.RFLX_Scalar_Sequence (RFLX.RFLX_Types, RFLX.Arrays.Enumeration, RFLX.Arrays.Enumeration_Base, RFLX.Arrays.Valid, RFLX.Arrays.To_Actual, RFLX.Arrays.To_Base);
