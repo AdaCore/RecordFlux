3a29a0fe2b7f0c83f990bf4b594863b5746a39ff
Tobias Reiher <reiher@componolit.com>
2020-08-25 16:21:25 +0200

    Remove redundant isinstance check
    Ref. #291, #409

--- rflx/model.py
+++ rflx/model.py
@@ -35,7 +35,6 @@ from rflx.expression import (
     Pow,
     ProofResult,
     Relation,
-    String,
     Sub,
     ValidChecksum,
     ValueRange,
@@ -1042,7 +1041,7 @@ class AbstractMessage(Type):
 
         def invalid_relation(left: TypeExpr, right: TypeExpr) -> bool:
             return (
-                (isinstance(left, Opaque) and not isinstance(right, (Opaque, Aggregate, String)))
+                (isinstance(left, Opaque) and not isinstance(right, (Opaque, Aggregate)))
                 or (isinstance(left, Array) and not isinstance(right, (Array, Aggregate)))
                 or (isinstance(left, Aggregate) and not isinstance(right, Composite))
             )
