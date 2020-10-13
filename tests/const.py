a8d0a62a1fa5f0b7371050db80e519049cd69a6d
Tobias Reiher <reiher@componolit.com>
2020-10-13 16:02:29 +0200

    Move test specifications into `tests/data/specs`
    Ref. #252

--- tests/const.py
+++ tests/const.py
@@ -4,8 +4,8 @@ TEST_DIR = Path("tests")
 DATA_DIR = TEST_DIR / "data"
 CAPTURED_DIR = DATA_DIR / "captured"
 FIXTURE_DIR = DATA_DIR / "fixtures"
+SPEC_DIR = DATA_DIR / "specs"
 IDE_DIR = TEST_DIR / "ide"
-SPEC_DIR = TEST_DIR / "specs"
 GENERATED_DIR = TEST_DIR / "spark" / "generated"
 
 EX_SPEC_DIR = Path("examples/specs")
