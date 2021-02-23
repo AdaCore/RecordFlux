69157c434c7682a831cc4389048b3404ed6f7820
Tobias Reiher <reiher@componolit.com>
2021-02-23 11:58:09 +0100

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Communications",
         "Topic :: Security",
         "Topic :: Software Development :: Build Tools",
