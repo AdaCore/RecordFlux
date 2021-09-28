467858f5384298072337bdfc1ae68cad0a86eb5a
Johannes Kliemann <kliemann@componolit.com>
2021-09-28 13:30:59 +0200

    Replace str with StrID in PyRFLX package API
    ref #778

--- rflx/pyrflx/package.py
+++ rflx/pyrflx/package.py
@@ -1,6 +1,7 @@
 from typing import Callable, Dict, Iterator, Mapping, Union
 
 from rflx.common import Base
+from rflx.identifier import StrID
 from rflx.pyrflx import PyRFLXError
 from rflx.pyrflx.typevalue import MessageValue
 
@@ -15,24 +16,25 @@ class Package(Base):
         return self.__name
 
     def new_message(
-        self, key: str, parameters: Mapping[str, Union[bool, int, str]] = None
+        self, key: StrID, parameters: Mapping[str, Union[bool, int, str]] = None
     ) -> MessageValue:
-        message = self.__messages[key].clone()
+        message = self.__messages[str(key)].clone()
         if parameters:
             message.add_parameters(parameters)
         return message
 
-    def set_message(self, key: str, value: MessageValue) -> None:
-        self.__messages[key] = value
+    def set_message(self, key: StrID, value: MessageValue) -> None:
+        self.__messages[str(key)] = value
 
-    def __getitem__(self, key: str) -> MessageValue:
+    def __getitem__(self, key: StrID) -> MessageValue:
         return self.new_message(key)
 
     def __iter__(self) -> Iterator[MessageValue]:
         return self.__messages.values().__iter__()
 
-    def set_checksum_functions(self, functions: Dict[str, Dict[str, Callable]]) -> None:
+    def set_checksum_functions(self, functions: Dict[StrID, Dict[str, Callable]]) -> None:
         for message_name, field_name_to_function_mapping in functions.items():
+            message_name = str(message_name)
             if message_name not in self.__messages:
                 raise PyRFLXError(f'"{message_name}" is not a message in {self.__name}')
             self.__messages[message_name].set_checksum_function(field_name_to_function_mapping)
