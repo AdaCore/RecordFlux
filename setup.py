ae818fea595b6e4b79f64140075ff1380ec1024a
Tobias Reiher <reiher@componolit.com>
2021-10-01 13:54:47 +0200

    Upgrade black, hypothesis, pytest-xdist and tqdm

--- setup.py
+++ setup.py
@@ -53,18 +53,18 @@ setup(
     ],
     extras_require={
         "devel": [
-            "black ==20.8b1",
+            "black ==21.9b0",
             "flake8 >=3.9.2, <3.10",
-            "hypothesis >=6.14, <6.15",
+            "hypothesis >=6.14, <6.24",
             "isort >=5.9.1, <5.10",
             "mypy ==0.910",
             "pyicontract-lint >=2.1.2, <2.2",
             "pylint >=2.8.3, <2.12",
             "pytest >=6.2.4, <6.3",
-            "ruamel.yaml >=0.17",
+            "ruamel.yaml >=0.17, <0.18",
             "pytest-cov >=2.12.1, <2.13",
-            "pytest-xdist >=2.3.0, <2.4",
-            "tqdm >=4.61.1, <4.62",
+            "pytest-xdist >=2.3.0, <2.5",
+            "tqdm >=4.61.1, <4.63",
             "types-pkg_resources >=0.1.3, <0.2",
         ]
     },
