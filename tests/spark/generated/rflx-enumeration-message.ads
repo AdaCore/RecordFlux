3f8e824e21a2682790b55840ec74de4a980a0949
Johannes Kanig <kanig@adacore.com>
2022-02-03 10:32:28 +0900

    Use True postcondition for Valid_Context to prevent inlining

--- tests/spark/generated/rflx-enumeration-message.ads
+++ tests/spark/generated/rflx-enumeration-message.ads
@@ -411,6 +411,8 @@ private
 
    pragma Warnings (Off, """Buffer"" is not modified, could be of access constant type");
 
+   pragma Warnings (Off, "postcondition does not mention function result");
+
    function Valid_Context (Buffer_First, Buffer_Last : RFLX_Types.Index; First : RFLX_Types.Bit_Index; Last : RFLX_Types.Bit_Length; Verified_Last : RFLX_Types.Bit_Length; Written_Last : RFLX_Types.Bit_Length; Buffer : RFLX_Types.Bytes_Ptr; Cursors : Field_Cursors) return Boolean is
      ((if Buffer /= null then Buffer'First = Buffer_First and Buffer'Last = Buffer_Last)
       and then (RFLX_Types.To_Index (First) >= Buffer_First
@@ -443,9 +445,14 @@ private
                 then
                    Cursors (F_Priority).Last - Cursors (F_Priority).First + 1 = RFLX.Enumeration.Priority_Base'Size
                    and then Cursors (F_Priority).Predecessor = F_Initial
-                   and then Cursors (F_Priority).First = First));
+                   and then Cursors (F_Priority).First = First))
+    with
+     Post =>
+       True;
 
    pragma Warnings (On, """Buffer"" is not modified, could be of access constant type");
+
+   pragma Warnings (On, "postcondition does not mention function result");
 
    type Context (Buffer_First, Buffer_Last : RFLX_Types.Index := RFLX_Types.Index'First; First : RFLX_Types.Bit_Index := RFLX_Types.Bit_Index'First; Last : RFLX_Types.Bit_Length := RFLX_Types.Bit_Length'First) is
       record
