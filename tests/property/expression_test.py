8c688f61cadc6227f2d31f2f47318cee334ea6ce
Tobias Reiher <reiher@adacore.com>
2022-07-14 17:56:48 +0200

    Add property test for expression conversion
    Ref. #691, #1106

---
+++ tests/property/expression_test.py
@@ -0,0 +1,50 @@
+from hypothesis import given, settings, strategies as st
+
+import rflx.expression as expr
+from tests.property import strategies
+
+
+@given(
+    st.one_of(
+        strategies.mathematical_expressions(
+            st.one_of(
+                strategies.numbers()
+                | strategies.variables(strategies.identifiers())
+                | strategies.attributes(strategies.identifiers())
+            )
+        ),
+        strategies.boolean_relations(
+            st.one_of(strategies.aggregates(strategies.numbers()) | strategies.strings()),
+        ),
+        strategies.boolean_expressions(
+            st.one_of(
+                strategies.mathematical_expressions(
+                    st.one_of(
+                        strategies.numbers()
+                        | strategies.variables(strategies.identifiers())
+                        | strategies.attributes(strategies.identifiers())
+                    )
+                )
+            )
+        ),
+        strategies.calls(
+            st.one_of(
+                strategies.numbers()
+                | strategies.variables(strategies.identifiers())
+                | strategies.attributes(strategies.identifiers())
+            )
+        ),
+        strategies.quantified_expressions(
+            st.one_of(
+                strategies.numbers()
+                | strategies.variables(strategies.identifiers())
+                | strategies.attributes(strategies.identifiers())
+            )
+        ),
+        strategies.strings(),
+        strategies.aggregates(strategies.numbers()),
+    )
+)
+@settings(deadline=None)
+def test_conversion(expression: expr.Expr) -> None:
+    assert expression.ada_expr().rflx_expr() == expression
