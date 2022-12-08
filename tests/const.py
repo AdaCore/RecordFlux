92c142532f6ae8890758a0359ca67d5f5740f6d2
Johannes Kliemann <kliemann@adacore.com>
2022-12-08 12:26:01 +0000

    Rename tests/features to tests/feature
    ref #1265

--- tests/const.py
+++ tests/const.py
@@ -7,7 +7,7 @@ FIXTURE_DIR = DATA_DIR / "fixtures"
 SPEC_DIR = DATA_DIR / "specs"
 IDE_DIR = TEST_DIR / "ide"
 GENERATED_DIR = TEST_DIR / "spark" / "generated"
-FEATURE_DIR = TEST_DIR / "features"
+FEATURE_DIR = TEST_DIR / "feature"
 
 EX_SPEC_DIR = Path("examples/specs")
 MAIN = "main.adb"
