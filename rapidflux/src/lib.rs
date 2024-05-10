49c257f1f5359ea19d3b45625eebb59c91c5fadb
Tobias Reiher <reiher@adacore.com>
2024-05-10 16:53:16 +0200

    Remove Tarpaulin remnants
    Ref. None

--- rapidflux/src/lib.rs
+++ rapidflux/src/lib.rs
@@ -10,7 +10,6 @@ use pyo3::prelude::*;
 mod error;
 
 #[pymodule]
-#[cfg(not(tarpaulin_include))]
 fn rapidflux(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<error::Location>()?;
     Ok(())
