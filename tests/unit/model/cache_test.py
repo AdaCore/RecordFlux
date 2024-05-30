8669c587bd4b1ac4c7525244c9e8e2b4b7876de5
Corentin Machu <machu@adacore.com>
2024-05-30 16:49:25 +0200

    Add cache locking system
    This fix cache race conditions when multiple instances of RecordFlux are
    running in the background.
    
    Ref. eng/recordflux/RecordFlux#1655

--- tests/unit/model/cache_test.py
+++ tests/unit/model/cache_test.py
@@ -5,7 +5,7 @@ import pytest
 from rflx import expression as expr, model
 from rflx.identifier import ID
 from rflx.model import cache
-from rflx.rapidflux import Location
+from rflx.rapidflux import Location, RecordFluxError
 from tests.data import models
 from tests.utils import assert_stderr_regex
 
@@ -118,3 +118,33 @@ def test_never_verify() -> None:
     assert c.is_verified(d)
     c.add_verified(d)
     assert c.is_verified(d)
+
+
+@pytest.mark.timeout(2)
+@pytest.mark.parametrize("load", [True, False])
+def test_cache_timeout(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, load: bool) -> None:
+    cache_path = tmp_path / "cache_path.json"
+    lock_file = cache_path.with_suffix(".lock")
+    lock_file.write_text("42")
+    monkeypatch.setattr(cache.FileLock, "LOCK_TIMEOUT", 0.1)
+    expected_regex = (
+        "^error: failed to acquire cache lock after 0.1 seconds\n"
+        'note: the cache is locked by a process with a PID of "42"\n'
+        "help: if the process that owns the lock isn't active anymore, deleting "
+        f'"{lock_file}" will solve this issue$'
+    )
+
+    def _patched_cache_init(self: cache.Cache, file: Path) -> None:
+        self._file = file
+        self._verified = {}
+
+    # Prevent the constructor to call _load_cache itself.
+    monkeypatch.setattr(cache.Cache, "__init__", _patched_cache_init)
+    c = cache.Cache(cache_path)
+
+    if load:
+        with pytest.raises(RecordFluxError, match=expected_regex):
+            c._load_cache()  # noqa: SLF001
+    else:
+        with pytest.raises(RecordFluxError, match=expected_regex):
+            c._write_cache()  # noqa: SLF001
