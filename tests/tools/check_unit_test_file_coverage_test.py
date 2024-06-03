b27be8447e81dafea074270c81563dff3c781283
Alexander Senier <senier@adacore.com>
2024-06-03 15:33:17 +0000

    Allow ignoring subdirectories in unit test file coverage check
    Ref. eng/recordflux/RecordFlux#1600

--- tests/tools/check_unit_test_file_coverage_test.py
+++ tests/tools/check_unit_test_file_coverage_test.py
@@ -34,104 +34,145 @@ def test_missing_source_dir(tmp_path: Path) -> None:
 
 
 @pytest.mark.parametrize(
-    ("source_files", "test_files", "expected"),
+    ("source_files", "test_files", "expected", "ignore"),
     [
-        ([], [], None),
-        (["unrelated.txt"], [], None),
-        (["subdir/unrelated.txt"], [], None),
-        (["__init__.py"], [], None),
-        (["subdir/__init__.py"], [], None),
-        (["unrelated.txt", "__init__.py"], [], None),
-        (["subdir/unrelated.txt", "__init__.py"], [], None),
-        (["unrelated.txt", "subdir/__init__.py"], [], None),
-        ([], ["unrelated.txt"], None),
-        ([], ["subdir/unrelated.txt"], None),
-        ([], ["__init__.py"], None),
-        ([], ["subdir/__init__.py"], None),
-        ([], ["unrelated.txt", "__init__.py"], None),
-        ([], ["unrelated.txt", "subdir/__init__.py"], None),
-        ([], ["subdir/unrelated.txt", "subdir/__init__.py"], None),
-        (["file1.py"], ["file1_test.py"], None),
-        (["file1.pyi"], ["file1_test.py"], None),
-        (["file1.py", "file1.pyi"], ["file1_test.py"], None),
-        (["subdir/file1.py"], ["subdir/file1_test.py"], None),
-        (["subdir/file1.pyi"], ["subdir/file1_test.py"], None),
-        (["subdir/file1.pyi", "subdir/file1.pyi"], ["subdir/file1_test.py"], None),
-        (["file1.py", "__init__.py"], ["file1_test.py"], None),
-        (["file1.py"], ["file1_test.py", "__init__.py"], None),
-        (["file1.py"], [], "no tests for: file1.py"),
-        (["subdir/file1.py"], [], "no tests for: subdir/file1.py"),
-        (["file1.py", "__init__.py"], [], "no tests for: file1.py"),
-        (["subdir/file1.py", "__init__.py"], [], "no tests for: subdir/file1.py"),
-        (["file1.py"], ["__init__.py"], "no tests for: file1.py"),
-        (["file1.py", "file2.py"], [], "no tests for: file1.py, file2.py"),
-        (["file1.py", "subdir/file2.py"], [], "no tests for: file1.py, subdir/file2.py"),
-        ([], ["file1_test.py"], "no corresponding source for: file1_test.py"),
-        ([], ["subdir/file1_test.py"], "no corresponding source for: subdir/file1_test.py"),
-        (["__init__.py"], ["file1_test.py"], "no corresponding source for: file1_test.py"),
+        ([], [], None, None),
+        (["unrelated.txt"], [], None, None),
+        (["subdir/unrelated.txt"], [], None, None),
+        (["__init__.py"], [], None, None),
+        (["subdir/__init__.py"], [], None, None),
+        (["unrelated.txt", "__init__.py"], [], None, None),
+        (["subdir/unrelated.txt", "__init__.py"], [], None, None),
+        (["unrelated.txt", "subdir/__init__.py"], [], None, None),
+        ([], ["unrelated.txt"], None, None),
+        ([], ["subdir/unrelated.txt"], None, None),
+        ([], ["__init__.py"], None, None),
+        ([], ["subdir/__init__.py"], None, None),
+        ([], ["unrelated.txt", "__init__.py"], None, None),
+        ([], ["unrelated.txt", "subdir/__init__.py"], None, None),
+        ([], ["subdir/unrelated.txt", "subdir/__init__.py"], None, None),
+        (["file1.py"], ["file1_test.py"], None, None),
+        (["file1.pyi"], ["file1_test.py"], None, None),
+        (["file1.py", "file1.pyi"], ["file1_test.py"], None, None),
+        (["subdir/file1.py"], ["subdir/file1_test.py"], None, None),
+        (["subdir/file1.pyi"], ["subdir/file1_test.py"], None, None),
+        (["subdir/file1.py", "subdir/file1.pyi"], ["subdir/file1_test.py"], None, None),
+        (["file1.py", "__init__.py"], ["file1_test.py"], None, None),
+        (["file1.py"], ["file1_test.py", "__init__.py"], None, None),
+        (["file1.py"], [], "no tests for: file1.py", None),
+        (["subdir/file1.py"], [], "no tests for: subdir/file1.py", None),
+        (["file1.py", "__init__.py"], [], "no tests for: file1.py", None),
+        (["subdir/file1.py", "__init__.py"], [], "no tests for: subdir/file1.py", None),
+        (["file1.py"], ["__init__.py"], "no tests for: file1.py", None),
+        (["file1.py", "file2.py"], [], "no tests for: file1.py, file2.py", None),
+        (["file1.py", "subdir/file2.py"], [], "no tests for: file1.py, subdir/file2.py", None),
+        ([], ["file1_test.py"], "no corresponding source for: file1_test.py", None),
+        ([], ["subdir/file1_test.py"], "no corresponding source for: subdir/file1_test.py", None),
+        (["__init__.py"], ["file1_test.py"], "no corresponding source for: file1_test.py", None),
         (
             ["__init__.py"],
             ["subdir/file1_test.py"],
             "no corresponding source for: subdir/file1_test.py",
+            None,
         ),
-        (["subdir/__init__.py"], ["file1_test.py"], "no corresponding source for: file1_test.py"),
-        ([], ["file1_test.py", "__init__.py"], "no corresponding source for: file1_test.py"),
-        ([], ["unrelated.py"], "no corresponding source for: unrelated.py"),
-        ([], ["subdir/unrelated.py"], "no corresponding source for: subdir/unrelated.py"),
+        (
+            ["subdir/__init__.py"],
+            ["file1_test.py"],
+            "no corresponding source for: file1_test.py",
+            None,
+        ),
+        ([], ["file1_test.py", "__init__.py"], "no corresponding source for: file1_test.py", None),
+        ([], ["unrelated.py"], "no corresponding source for: unrelated.py", None),
+        ([], ["subdir/unrelated.py"], "no corresponding source for: subdir/unrelated.py", None),
         (
             [],
             ["file1_test.py", "file2_test.py"],
             "no corresponding source for: file1_test.py, file2_test.py",
+            None,
         ),
         (
             [],
             ["subdir/file1_test.py", "file2_test.py"],
             "no corresponding source for: file2_test.py, subdir/file1_test.py",
+            None,
         ),
         (
             ["file1.py"],
             ["file1_test.py", "file2_test.py"],
             "no corresponding source for: file2_test.py",
+            None,
         ),
         (
             ["subdir/file1.py"],
             ["subdir/file1_test.py", "file2_test.py"],
             "no corresponding source for: file2_test.py",
+            None,
+        ),
+        (
+            ["subdir/file1.py"],
+            [],
+            None,
+            ["subdir"],
+        ),
+        (
+            [],
+            ["subdir/file1_test.py"],
+            None,
+            ["subdir"],
+        ),
+        (
+            ["subdir/file1.py", "subdir2/file2.py"],
+            ["subdir/file1_test.py", "file2_test.py"],
+            "no corresponding source for: file2_test.py",
+            ["subdir2"],
+        ),
+        (
+            ["subdir/file1.py", "subdir2/file2.py"],
+            ["subdir/file1_test.py", "subdir3/file2_test.py"],
+            "no tests for: subdir2/file2.py",
+            ["subdir3"],
         ),
         (
             ["file1.py"],
             ["file1_test.py", "subdir/file2_test.py"],
             "no corresponding source for: subdir/file2_test.py",
+            None,
         ),
         (
             ["file1.py"],
             ["file2_test.py"],
             "no tests for: file1.py\nno corresponding source for: file2_test.py",
+            None,
         ),
         (
             ["subdir/file1.py"],
             ["file2_test.py"],
             "no tests for: subdir/file1.py\nno corresponding source for: file2_test.py",
+            None,
         ),
         (
             ["file1.py"],
             ["subdir/file2_test.py"],
             "no tests for: file1.py\nno corresponding source for: subdir/file2_test.py",
+            None,
         ),
         (
             ["file1.py", "file2.py"],
             ["file2_test.py", "file3_test.py"],
             "no tests for: file1.py\nno corresponding source for: file3_test.py",
+            None,
         ),
         (
             ["file1.py", "subdir/file2.py"],
             ["subdir/file2_test.py", "file3_test.py"],
             "no tests for: file1.py\nno corresponding source for: file3_test.py",
+            None,
         ),
         (
             ["subdir/file1.py", "file2.py"],
             ["file2_test.py", "subdir/file3_test.py"],
             "no tests for: subdir/file1.py\nno corresponding source for: subdir/file3_test.py",
+            None,
         ),
     ],
 )
@@ -140,6 +181,7 @@ def test_no_files(
     source_files: list[str],
     test_files: list[str],
     expected: Optional[str],
+    ignore: Optional[list[str]],
 ) -> None:
     source_dir = tmp_path / "source"
     test_dir = tmp_path / "test"
@@ -163,16 +205,50 @@ def test_no_files(
             check_unit_test_file_coverage.check_file_coverage(
                 source_dir=source_dir,
                 test_dir=test_dir,
+                ignore=None if ignore is None else [Path(i) for i in ignore],
             )
     else:
-        check_unit_test_file_coverage.check_file_coverage(source_dir=source_dir, test_dir=test_dir)
+        check_unit_test_file_coverage.check_file_coverage(
+            source_dir=source_dir,
+            test_dir=test_dir,
+            ignore=None if ignore is None else [Path(i) for i in ignore],
+        )
+
+
+@pytest.mark.parametrize(
+    ("file", "ignore_list", "expected"),
+    [
+        ("a", ["b", "c", "d"], False),
+        ("b", ["b", "c", "d"], True),
+        ("a", ["a/b", "a/c", "a/d"], False),
+        ("a/b/c", ["a"], True),
+        ("a/b/c", ["a/b"], True),
+        ("a/b/c", ["a", "b", "c"], True),
+        ("a/b/c", ["a/b", "b", "c"], True),
+        ("a/b/c", ["a/b/c", "b", "c"], True),
+        ("a", [], False),
+        ("a", None, False),
+    ],
+)
+def test_ignored(file: str, ignore_list: Optional[list[str]], expected: bool) -> None:
+    assert (
+        check_unit_test_file_coverage._ignored(  # noqa: SLF001
+            Path(file),
+            [Path(i) for i in ignore_list] if ignore_list else None,
+        )
+        == expected
+    )
 
 
 def test_cli(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> None:
     stored_source_dir: Optional[Path] = None
     stored_test_dir: Optional[Path] = None
 
-    def dummy_check_file_coverage(source_dir: Path, test_dir: Path) -> None:
+    def dummy_check_file_coverage(
+        source_dir: Path,
+        test_dir: Path,
+        ignore: Optional[list[Path]] = None,  # noqa: ARG001
+    ) -> None:
         nonlocal stored_source_dir, stored_test_dir
         stored_source_dir = source_dir
         stored_test_dir = test_dir
--- tools/check_unit_test_file_coverage.py
+++ tools/check_unit_test_file_coverage.py
@@ -12,18 +12,32 @@ from __future__ import annotations
 import argparse
 import sys
 from pathlib import Path
+from typing import Optional
 
 
 class CheckUnitTestFileError(Exception):
     pass
 
 
-def check_file_coverage(source_dir: Path, test_dir: Path) -> None:
+def _ignored(file: Path, ignore_list: Optional[list[Path]] = None) -> bool:
+    return ignore_list is not None and any(
+        i.parts == file.parts[: len(i.parts)] for i in ignore_list
+    )
+
+
+def check_file_coverage(
+    source_dir: Path,
+    test_dir: Path,
+    ignore: Optional[list[Path]] = None,
+) -> None:
+
     if not source_dir.exists():
         raise CheckUnitTestFileError(f"missing source directory: {source_dir}")
 
     if not test_dir.exists():
         raise CheckUnitTestFileError(f"missing test directory: {test_dir}")
+
+    ignore = ignore or []
 
     source_files = [
         s.relative_to(source_dir) for ext in ("**/*.py", "**/*.pyi") for s in source_dir.glob(ext)
@@ -34,7 +48,8 @@ def check_file_coverage(source_dir: Path, test_dir: Path) -> None:
         str(source_file)
         for source_file in source_files
         if (
-            source_file.name != "__init__.py"
+            not _ignored(source_file, ignore)
+            and source_file.name not in ("__init__.py", "__init__.pyi")
             and source_file.with_name(source_file.stem + "_test.py") not in test_files
         )
     ]
@@ -43,7 +58,8 @@ def check_file_coverage(source_dir: Path, test_dir: Path) -> None:
         str(test_file)
         for test_file in test_files
         if (
-            test_file.name != "__init__.py"
+            not _ignored(test_file, ignore)
+            and test_file.name not in ("__init__.py", "__init__.pyi")
             and (
                 not test_file.stem.endswith("_test")
                 or (
@@ -69,10 +85,11 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--source-dir", type=Path, required=True, help="Source directory")
     parser.add_argument("--test-dir", type=Path, required=True, help="Unit test directory")
+    parser.add_argument("--ignore", type=Path, action="append", help="Directory to ignore")
     args = parser.parse_args()
 
     try:
-        check_file_coverage(source_dir=args.source_dir, test_dir=args.test_dir)
+        check_file_coverage(source_dir=args.source_dir, test_dir=args.test_dir, ignore=args.ignore)
     except CheckUnitTestFileError as e:
         sys.exit(f"{e}")
 
