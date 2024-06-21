2a97de264147eb0f51289c4bfd5d1f6c7917015b
Corentin Machu <machu@adacore.com>
2024-06-21 14:32:36 +0200

    Disable quiet mode when testing by default
    Ref. eng/recordflux/RecordFlux#1699

--- tests/data/fixtures/rapidflux.py
+++ tests/data/fixtures/rapidflux.py
@@ -1,11 +1,16 @@
 import pytest
 
-from rflx.rapidflux import RecordFluxError, source_code
+from rflx.rapidflux import RecordFluxError, logging, source_code
 
 
 @pytest.fixture(autouse=True)
 def _clear_stored_source_code() -> None:
     source_code.clear()
+
+
+@pytest.fixture(autouse=True)
+def _clear_quiet_logging() -> None:
+    logging.set_quiet(False)
 
 
 @pytest.fixture()
